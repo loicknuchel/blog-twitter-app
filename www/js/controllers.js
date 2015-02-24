2478140d7e38116c7ec66432b57f9c5bab2d6e29
Loïc Knuchel <loicknuchel@gmail.com>
2015-02-24 23:09:21 +0100

    step3 : add infinite-scroll et refresher on twitt list

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,4 +5,22 @@ angular.module('app')
   TwittSrv.getTwitts().then(function(twitts){
     $scope.twitts = twitts;
   });
+
+  $scope.doRefresh = function(){
+    TwittSrv.getNewTwitts().then(function(newTwitts){
+      $scope.twitts = newTwitts.concat($scope.twitts);
+    }).finally(function() {
+      // Stop the ion-refresher from spinning
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
+
+  $scope.loadMore = function(){
+    TwittSrv.getMoreTwitts().then(function(olderTwitts){
+      $scope.twitts = $scope.twitts.concat(olderTwitts);
+    }).finally(function() {
+      // Stop the ion-infinite-scroll from spinning
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
 });
